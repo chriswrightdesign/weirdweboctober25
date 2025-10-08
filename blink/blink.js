909031436bdb80d5f153bb518b4ce569c242f3ff
Christopher Wright <chrisw@campaignmonitor.com>
2025-10-09 06:16:18 +1100

    Blink!

---
+++ blink/blink.js
@@ -0,0 +1,57 @@
+(() => {
+    const body = document.body;
+    const start = document.querySelector('.js-start');
+    const trap = document.querySelector('.js-trap');
+    const stage = document.querySelector('.js-stage');
+    const circle = document.querySelector('.js-circle');
+    const status = document.querySelector('.js-output');
+
+    let isActive = false;
+    let isPending = false;
+
+    const inititiate = () => {
+        begin();
+        clearTimeout();
+        body.classList.add('is-started');
+        // add a timer for a random amount between 3 and 9 seconds
+        const timer = Math.floor(Math.random() * 6000) + 3000;
+        isPending = true;
+        setTimeout(() => {
+            if (!isPending) {
+                return;
+            }
+            stage.classList.add('is-active');
+            isActive = true;
+        }, Math.floor(Math.random() * 6000) + 3000);
+    }
+
+    const end = () => {
+        isActive = false;
+        isPending = false;
+        body.classList.remove('is-started');
+        stage.classList.remove('is-active');
+        trap.disabled = true;
+    }
+
+    const begin = () => {
+        trap.disabled = false;
+    }
+
+    circle.addEventListener('animationend', () => {
+        end();
+    });
+
+    trap.addEventListener('click', () => {
+        if (isActive) {
+            status.textContent = 'You win!';
+            end();
+        } else if (isPending) {
+            status.textContent = 'Nope';
+            end();
+        }
+    });
+    start.addEventListener('click', () => {
+        inititiate();
+        
+    });
+})();
